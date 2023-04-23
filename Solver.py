6c64b04e262cfca8260cd9476b857d6fc53787b9
Michael <michaeltheisen@hotmail.com>
2023-04-23 15:32:47 -0700

    got it working, fixing input methods

--- Solver.py
+++ Solver.py
@@ -1,3 +1,5 @@
+import Board
+#import Tester
 # " Source code that implements BFS, DFS, GBFS, AStar to solve n-by-n sliding puzzle."
 
 # Under the assumption that we do not have to worry about checking if the puzzle is 
@@ -5,7 +7,23 @@
 # We will save that for later, after ensuring proper documentation.
 
 # Breadth First Search based on textbook page 82.
-def BFS(problem):
+
+
+def BFS(initial, goal):
+    visited = set([tuple(map(tuple, initial))])
+    queue = [(initial, 0)]
+    while queue:
+        state, depth = queue.pop(0)
+        if Board.getStateString(state) == Board.getStateString(goal):
+            return depth
+        for neighbor in Board.getNeighbors(state):
+            if tuple(map(tuple, neighbor)) not in visited:
+                visited.add(tuple(map(tuple, neighbor)))
+                queue.append((neighbor, depth+1))
+    return -1
+
+
+#def BFS(problem):
 
 
     # node = node with state = problem.InitialState, PathCost = 0
@@ -24,7 +42,7 @@ def BFS(problem):
     #         if childe.state is not in explorer or frontier then
     #             if problem.goaltest(child.state) then
     #                 return solution
-    return #solution
+    #return #solution
 
 # Depth First Search based on textbook page 88.
 def DFS():
--- Tester.py
+++ Tester.py
@@ -1,3 +1,5 @@
+import Board
+import Solver 
 # "A tester file that connects your Board and Solver code."
 #
 #
@@ -9,45 +11,135 @@
 # for (n = 2), the solution = ("213 ") ALWAYS. Rows move to the left in ascending order, but space is always BOTTOM RIGHT.
 # for (n = 3), the solution = (" 12345678") ALWAYS. Rows move to the right in ascending order, but space is always TOP LEFT.
 # for (n = 4), the solution = ("123456789ABCDEF ") ALWAYS. Rows move to the right in ascending order, but space is always BOTTOM RIGHT.
+# 
 # EXTRA CREDIT
 # For (n > 4), the solution = ("(n*n)-1") I assume, rows moving right in ascending order, but where should the space be? Indication is not given. I will assume BOTTOM RIGHT.
 # implementation of unsolvability is last if time. It is not necessary.
 #
 ################################
-size = 0
-initial = 0
-goal = 0
-searchmethod = "empty"
-depth = 0
+#size = 0
+#initial = 0
+#goal = 0
+#searchmethod = "empty"
+#depth = 0
 numCreated = 0
 numExpanded = 0
 maxFringe = 0
+
+
+
+def printState(size, initial, goal, searchmethod, depth):
+    initialStr = "".join(str(i) for row in initial for i in row)
+    goalStr = "".join(str(i) for row in goal for i in row)
+
+    print(f'{"size:"} + {size}')
+    print(f'{"initial"} + {initialStr}"')
+    print(f'{"goal: "} + {goalStr}"')  
+    print(f'{searchmethod}')
+    print(f'{depth}')
+
 
 # Print input request to console
 file = open('Readme.txt', 'a')
 print()
 print("Type 'q' to exit and print to Readme.txt file.\nEnter Input of the form: [size] " + '"[initial state]" ' + "[search method]")
 print()
-prompt_input = input("Input: ")
-while prompt_input != 'q':
+promptInput = input("Input: ")
+size, newInitial, searchmethod = promptInput.split()
+# Convert the input string to a list of characters
+n=size
+charList = list(newInitial)
+
+# Define an empty list to hold the sublists
+initial = []
+
+# Loop through the characters and group them into sublists of four
+for i in range(0, len(charList), n):
+    charInitial = charList[i:i+n]
+    initial.append(charInitial)
+
+
+# Checking constant goal state for n = 2 per required Goal State on specification.
+if n == 2:
+    # initial = [
+    #     ['3', '2'],
+    #     [' ', '1'],
+    # ]
+    goal = [
+        ['2', '1'],
+        ['3', ' '],
+    ]
+
+# Checking constant goal state for n = 3 per required Goal State on specification.
+if n == 3:
+    # initial = [
+    #     ['4', '7', '3'],
+    #     ['1', '5', '8'],
+    #     ['6', '2', ' ']
+    # ]
+    goal = [
+        [' ', '1', '2'],
+        ['3', '4', '5'],
+        ['6', '7', '8']
+    ]
+
+# Checking constant goal state for n = 4 per required Goal State on specification.
+if n == 4:
+    # initial = [
+    #     ['1', '2', '3', '4'],
+    #     ['5', '6', '7', '8'],
+    #     ['9', 'A', 'B', ' '],
+    #     ['D', 'E', 'F', 'C']
+    # ]
+    goal = [
+        ['1', '2', '3', '4'],
+        ['5', '6', '7', '8'],
+        ['9', 'A', 'B', 'C'],
+        ['D', 'E', 'F', ' ']
+    ]
+# Since no specification for goal state above 4 was given, I will simply just 
+# adopt "Row Order, Space in Bottom Right" similar to that of n = 4 for all puzzles
+# of size n > 4.
+# if n > 4:
+
+searchmethod = "BFS"
+depth = Solver.BFS(initial, goal)
+solution = Solver.BFS(initial, goal)
+
+
+    
+while promptInput != 'q':
     print()
     # Print output to console
-    print("The output for now is the input so, : " + prompt_input)
-    print("size: " + str(size))
-    print("initial: " + '"' + str(initial) + '"')
-    print("goal: " + '"' + str(goal) + '"')
-    print("searchmethod: " + str(searchmethod))
-    print(str(depth) + ", " + str(numCreated) + ", " + str(numExpanded) + ", " + str(maxFringe))
-    print("----------------")
-    print()
+    # print("size: " + str(size))
+    # print("initial: " + '"' + str(initial) + '"')
+    # print("goal: " + '"' + str(goal) + '"')
+    # print("searchmethod: " + str(searchmethod))
+    # print(str(depth) + ", " + str(numCreated) + ", " + str(numExpanded) + ", " + str(maxFringe))
+    # print("----------------")
+    # print()
 
-    # Print to a Readme.txt
-    file.write("size: " + str(size))
-    file.write("\ninitial: " + '"' + str(initial) + '"')
-    file.write("\ngoal: " + '"' + str(goal) + '"')
-    file.write("\nsearchmethod: " + str(searchmethod))
-    file.write("\n" + str(depth) + ", " + str(numCreated) + ", " + str(numExpanded) + ", " + str(maxFringe))
-    file.write("\n----------------\n")
 
-    prompt_input = input("Input: ")
+    promptInput = input("Input: ")
+    size, newinitial, searchmethod = promptInput.split()    
+    if solution:
+        printState(size, initial, goal, searchmethod, depth)
+        print()
+        # Print output to console
+        # print("size: " + str(size))
+        # print("initial: " + '"' + str(initial) + '"')
+        # print("goal: " + '"' + str(goal) + '"')
+        # print("searchmethod: " + str(searchmethod))
+        # print(str(depth) + ", " + str(numCreated) + ", " + str(numExpanded) + ", " + str(maxFringe))
+        # print("----------------")
+        # print()
+        # Print to a Readme.txt
+        file.write("size: " + str(size))
+        file.write("\ninitial: " + '"' + str(initial) + '"')
+        file.write("\ngoal: " + '"' + str(goal) + '"')
+        file.write("\nsearchmethod: " + str(searchmethod))
+        file.write("\n" + str(depth) + ", " + str(numCreated) + ", " + str(numExpanded) + ", " + str(maxFringe))
+        file.write("\n----------------\n")
+    else:
+        print("No solution found")
 
